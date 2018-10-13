1aba6c225c72e34720b2750a28c7436ccd1befe3
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-13 14:59:10 -0500

    chore: clase tamal

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio01/Tamal.java
@@ -0,0 +1,53 @@
+package co.edu.sena.ejercicios.ejercicio01;
+
+public class Tamal {
+    private String color;
+    private String tamanio;
+    private String region;
+    private float precio;
+    private float peso;
+
+    public Tamal() {
+        //
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+}
