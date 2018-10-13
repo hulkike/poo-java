522c7c6a13d36d2d33683ddb99b144f5d76dced7
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-13 15:11:03 -0500

    chore: clase Pan

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio01/Pan.java
@@ -0,0 +1,53 @@
+package co.edu.sena.ejercicios.ejercicio01;
+
+public class Pan {
+    private String tipo;
+    private float precio;
+    private String color;
+    private float peso;
+    private String sabor;
+
+    public Pan() {
+        super();
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+}
