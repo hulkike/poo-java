d4c4f29546a4a01093de38fa363a14e0bb36e5db
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-13 15:09:19 -0500

    chore: clase Persona

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio01/Persona.java
@@ -0,0 +1,53 @@
+package co.edu.sena.ejercicios.ejercicio01;
+
+public class Persona {
+    private String colorOjos;
+    private String colorPiel;
+    private int dedos;
+    private String colorCabello;
+    private int lunares;
+
+    public Persona() {
+        super();
+    }
+
+    public String getColorOjos() {
+        return colorOjos;
+    }
+
+    public void setColorOjos(String colorOjos) {
+        this.colorOjos = colorOjos;
+    }
+
+    public String getColorPiel() {
+        return colorPiel;
+    }
+
+    public void setColorPiel(String colorPiel) {
+        this.colorPiel = colorPiel;
+    }
+
+    public int getDedos() {
+        return dedos;
+    }
+
+    public void setDedos(int dedos) {
+        this.dedos = dedos;
+    }
+
+    public String getColorCabello() {
+        return colorCabello;
+    }
+
+    public void setColorCabello(String colorCabello) {
+        this.colorCabello = colorCabello;
+    }
+
+    public int getLunares() {
+        return lunares;
+    }
+
+    public void setLunares(int lunares) {
+        this.lunares = lunares;
+    }
+}
