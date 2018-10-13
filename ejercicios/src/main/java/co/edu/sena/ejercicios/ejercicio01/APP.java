4f8eab04f87443283015915fd3eadb2ec72400e0
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-13 15:15:42 -0500

    chore: app del ejercicio 1

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio01/APP.java
@@ -0,0 +1,12 @@
+package co.edu.sena.ejercicios.ejercicio01;
+
+public class APP {
+    public static void main(String[] args) {
+        Arbol arbol= new Arbol();
+        Avion avion=new Avion();
+        Gafas gafas= new Gafas();
+        Pan pan = new Pan();
+        Persona persona= new Persona();
+        Tamal tamal= new Tamal();
+    }
+}
