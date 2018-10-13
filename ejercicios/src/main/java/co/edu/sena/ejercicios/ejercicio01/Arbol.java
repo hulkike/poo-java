947750de414a9c7a6b996b98d6780f7a14adee77
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-13 15:03:13 -0500

    chore: clase Arbol

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio01/Arbol.java
@@ -0,0 +1,53 @@
+package co.edu.sena.ejercicios.ejercicio01;
+
+public class Arbol {
+    private String color;
+    private float altura;
+    private int edad;
+    private float diametro;
+    private String tipo;
+
+    public Arbol() {
+        super();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(float diametro) {
+        this.diametro = diametro;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
