688672aff95470d61305d2538c16a23ef18217d7
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-13 15:06:25 -0500

    chore: clase Gafas

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio01/Gafas.java
@@ -0,0 +1,53 @@
+package co.edu.sena.ejercicios.ejercicio01;
+
+public class Gafas {
+    private String marca;
+    private float precio;
+    private String color;
+    private float grosor;
+    private String estilo;
+
+    public Gafas() {
+        super();
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getGrosor() {
+        return grosor;
+    }
+
+    public void setGrosor(float grosor) {
+        this.grosor = grosor;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+}
