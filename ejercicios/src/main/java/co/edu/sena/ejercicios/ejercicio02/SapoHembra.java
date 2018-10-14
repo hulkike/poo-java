d63889485ed60f71a4d3cc1708706154531e433b
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-14 18:16:42 -0500

    chore: ejercicio02 clase SapoHembra

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/SapoHembra.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class SapoHembra extends Sapo {
+    public void criar(){
+        System.out.println("pongo huevos y los cargo hasta que eclosionan algunas veces los dejo botados");
+    }
+}
