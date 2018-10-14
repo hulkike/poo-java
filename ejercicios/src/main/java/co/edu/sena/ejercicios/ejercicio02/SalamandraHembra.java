98949aa1f4cca371898148a91204f3a856a49b16
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-14 18:21:15 -0500

    chore: ejercicio02 clase SalamandraHembra

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/SalamandraHembra.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class SalamandraHembra extends Salamandra {
+    public void criar(){
+        System.out.println("pongo huevos y los cuido hasta que eclosionen");
+    }
+}
