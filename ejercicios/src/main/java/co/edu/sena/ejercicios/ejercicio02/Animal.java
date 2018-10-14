3d9d275731b49a2077ae46d6f503b6d0a7442557
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-14 18:06:42 -0500

    chore: ejercicio02 clase Animal

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/Animal.java
@@ -0,0 +1,12 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public abstract class Animal {
+    public abstract void respirar();
+    public void morir(){
+        System.out.println("me mori");
+    }
+    public abstract void comer();
+    public abstract void desplazarse();
+    public abstract void reproducirse();
+    public abstract void nacer();
+}
