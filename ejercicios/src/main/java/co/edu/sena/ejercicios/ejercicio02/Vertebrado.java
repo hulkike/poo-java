bb3cd1800623191ea7611e0b9067960bec050c6d
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-14 18:08:23 -0500

    chore: ejercicio02 clase Vertebrado

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/Vertebrado.java
@@ -0,0 +1,8 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class Vertebrado extends Animal {
+    @Override
+    public void respirar() {
+        System.out.println("respiro oxigeno");
+    }
+}
