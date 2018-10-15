24da9312e329070dcbd250919a8cddb5cb6da47e
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-15 09:43:50 -0500

    chore: ejercicio02 clase PerroHembra

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/PerroHembra.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class PerroHembra extends Perro {
+    public void criar(){
+        System.out.println("cuida de mis cachorros y les amamanto");
+    }
+}
