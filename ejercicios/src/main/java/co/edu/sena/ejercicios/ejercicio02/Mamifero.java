97179ab39b61e2ae6ddcea78d5b2d5e1909a540f
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-15 09:39:16 -0500

    chore: ejercicio02 clase Mamifero

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/Mamifero.java
@@ -0,0 +1,18 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class Mamifero extends Vertebrado {
+    @Override
+    public void nacer() {
+        System.out.println("nazco del utero de mi madre");
+    }
+
+    @Override
+    public void comer() {
+        System.out.println("soy omnívoro");
+    }
+
+    @Override
+    public void reproducirse() {
+        System.out.println("de forma sexual");
+    }
+}
