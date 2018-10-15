a7ad4c28768974f30e26d08e7f81b48f37cf276d
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-15 09:45:53 -0500

    chore: ejercicio02 clase Humano

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/Humano.java
@@ -0,0 +1,8 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class Humano extends Mamifero {
+    @Override
+    public void desplazarse() {
+        System.out.println("soy bípedo");
+    }
+}
