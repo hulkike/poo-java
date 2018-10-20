ceb13212f45ff0c0aba565a2de667a7b80bab31c
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-19 22:29:58 -0500

    chore: ejercicio 02 clase Reptil

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/Reptil.java
@@ -0,0 +1,18 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class Reptil extends Vertebrado {
+    @Override
+    public void desplazarse() {
+        System.out.println("me desplazo caminando");
+    }
+
+    @Override
+    public void reproducirse() {
+        System.out.println("me reproduzco de forma sexual");
+    }
+
+    @Override
+    public void nacer() {
+        System.out.println("nazco de un huevo");
+    }
+}
