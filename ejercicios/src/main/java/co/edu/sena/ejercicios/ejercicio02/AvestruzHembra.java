50c075a012c748e330f1ecea3e6fe1ea038b011e
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-20 17:08:03 -0500

    chore: ejercicio 02 clase AvestruzHembra

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/AvestruzHembra.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class AvestruzHembra extends Avestruz {
+    public void criar(){
+        System.out.println("pongo huevos y cuido a mis crias hasta que crezcan lo suficiente");
+    }
+}
