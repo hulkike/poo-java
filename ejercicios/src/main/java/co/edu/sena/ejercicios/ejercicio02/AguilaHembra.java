a920e56fdadcd73f4857c9b5df1ad6ae896cef4c
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-20 17:03:35 -0500

    chore: ejercicio 02 clase AguilaHembra

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/AguilaHembra.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class AguilaHembra extends Aguila {
+    public void criar(){
+        System.out.println("pongo huevos y cuido mis crias hasta que puedan volar y cazar");
+    }
+}
