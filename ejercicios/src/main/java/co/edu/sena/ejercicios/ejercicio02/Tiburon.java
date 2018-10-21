23dc2e8d951d909964c2049ff623169556072b8e
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-21 10:02:15 -0500

    chore: ejercicio 02 clase TiburonHembra

--- ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/Tiburon.java
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/Tiburon.java
@@ -5,4 +5,9 @@ public class Tiburon extends Pez {
     public void comer() {
         System.out.println("soy carnívoro");
     }
+
+    @Override
+    public void nacer() {
+        System.out.println("no nacemos de huevos estamos en el vientre de nuestra madre a veces nos comemos a nuestros hermanos");
+    }
 }
---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/TiburonHembra.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio02;
+
+public class TiburonHembra extends Tiburon {
+    public void criar(){
+        System.out.println("crio a mis pequeños en mi vientre hasta que nacen");
+    }
+}
