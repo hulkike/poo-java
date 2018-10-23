8a17443cd02660057f458b60a4d069e831a2b106
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-23 17:18:37 -0500

    chore: ejercicio 03 clase Play4Slim

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Play4Slim.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Play4Slim extends Play4 {
+    public Play4Slim(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
