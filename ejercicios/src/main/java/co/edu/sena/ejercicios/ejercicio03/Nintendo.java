726281fddacef701323436a1e2e375b2493653be
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:26:27 -0500

    chore: ejercicio 03 clase Nintendo

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Nintendo.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Nintendo extends Consola {
+    public Nintendo(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
