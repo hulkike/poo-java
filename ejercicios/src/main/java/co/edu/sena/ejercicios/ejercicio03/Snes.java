64f8353474448e39bb45596d1fd7bd739cf311c6
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:27:40 -0500

    chore: ejercicio 03 clase Snes

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Snes.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Snes extends Nintendo {
+    public Snes(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
