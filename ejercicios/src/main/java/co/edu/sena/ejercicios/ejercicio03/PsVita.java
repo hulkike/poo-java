dd392b122257e8ff253fdce2f4a2595727ed3535
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:30:45 -0500

    chore: ejercicio 03 clase PsVita

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/PsVita.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class PsVita extends PlayStation {
+    public PsVita(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
