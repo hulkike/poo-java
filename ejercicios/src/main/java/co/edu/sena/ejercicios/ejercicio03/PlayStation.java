eaf77fb1dda735a73f08747ab085e4101027a9b0
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:29:44 -0500

    chore: ejercicio 03 clase PlayStation

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/PlayStation.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class PlayStation extends Consola {
+    public PlayStation(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
