9553ffeba132fd3dad5b50079ca8e5d4ec27ee23
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:29:00 -0500

    chore: ejercicio 03 clase Switch

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Switch.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Switch extends Nintendo {
+    public Switch(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
