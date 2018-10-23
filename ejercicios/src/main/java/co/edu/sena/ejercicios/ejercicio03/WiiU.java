b667241309d748afce50d04b80bcb8927403f631
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:27:10 -0500

    chore: ejercicio 03 clase WiiU

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/WiiU.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class WiiU extends Nintendo {
+    public WiiU(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
