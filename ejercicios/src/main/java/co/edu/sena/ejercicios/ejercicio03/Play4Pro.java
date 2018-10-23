5eb6fa806ed25aaca60e252cce989f712efe0ef4
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-23 17:17:59 -0500

    chore: ejercicio 03 clase Play4Pro

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Play4Pro.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Play4Pro extends Play4 {
+    public Play4Pro(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
