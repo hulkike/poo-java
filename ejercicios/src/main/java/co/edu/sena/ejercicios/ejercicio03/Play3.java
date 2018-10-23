17b7ac48ee10d96711866ee897057f32ae6e29d1
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:32:05 -0500

    chore: ejercicio 03 clase Play3

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Play3.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Play3 extends Play4 {
+    public Play3(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
