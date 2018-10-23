84493da77f0ea56e5a18a4ed2c7b4fb991c4c1a9
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-22 19:28:17 -0500

    chore: ejercicio 03 clase Wii

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Wii.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Wii extends Nintendo {
+    public Wii(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
