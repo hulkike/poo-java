4ad3bfb0b141bfe22d5fb1dcec2be6619f6cf53a
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-23 17:19:15 -0500

    chore: ejercicio 03 clase Play4Fat

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio03/Play4Fat.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio03;
+
+public class Play4Fat extends Play4 {
+    public Play4Fat(int controles, String conectorSonido, String conectorVideo) {
+        super(controles, conectorSonido, conectorVideo);
+    }
+}
