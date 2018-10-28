2248c34c967c7119e21d1d636d5a629565f71123
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-27 21:15:11 -0500

    chore: ejercicio 04 clase Iphone6

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio04/Iphone6.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio04;
+
+public class Iphone6 extends Apple {
+    public void tomarFotos(){
+        System.out.println("toma foto a 10 megapixeles");
+    }
+}
