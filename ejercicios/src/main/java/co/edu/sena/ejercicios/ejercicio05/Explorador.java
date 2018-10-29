9fd20de9d3395e9ed396583e3edc413fff56cac1
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-28 20:14:04 -0500

    chore: ejercicio 05 interface Explorador

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio05/Explorador.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio05;
+
+public interface Explorador extends HormigaInterface {
+    public void investigar();
+    public void marcarCaminos();
+    public void informar();
+}
