6674999bf096ec9f659080af1253bfd6c0e6c1bf
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-28 20:11:55 -0500

    chore: ejercicio 05 interface Obrero

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio05/Obrero.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio05;
+
+public interface Obrero extends HormigaInterface {
+    public void construir();
+    public void arreglar();
+    public void recolectar();
+    public void excavar();
+    public void cuidarCrias();
+}
