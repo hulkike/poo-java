4db0e99a7a3984245c44c27074cf773765d6784d
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-30 18:06:28 -0500

    chore: ejercicio 05 clase Reina

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio05/Reina.java
@@ -0,0 +1,11 @@
+package co.edu.sena.ejercicios.ejercicio05;
+
+public class Reina extends Hormiga {
+    public void ponerHuevos(){
+        System.out.println("pongo pongo huevitos");
+    }
+
+    public void mandar(){
+        System.out.println("mando a todas las hormigas");
+    }
+}
