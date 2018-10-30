a8e5726dc5365dbda6fc20b18624f60a72d43af3
Enrique Moreno <hemoreno33@misena.edu.co>
2018-10-30 18:04:47 -0500

    chore: ejercicio 05 clase Hormiga

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio05/Hormiga.java
@@ -0,0 +1,27 @@
+package co.edu.sena.ejercicios.ejercicio05;
+
+public class Hormiga {
+    public void comer(){
+        System.out.println("como como como");
+    }
+
+    public void respirar(){
+        System.out.println("erspiro aire");
+    }
+
+    public void caminar(){
+        System.out.println("camino camino");
+    }
+
+    public void correr(){
+        System.out.println("corro corro");
+    }
+
+    public void nacer(){
+        System.out.println("nazco de un huevo");
+    }
+
+    public void morir(){
+        System.out.println("me mori");
+    }
+}
