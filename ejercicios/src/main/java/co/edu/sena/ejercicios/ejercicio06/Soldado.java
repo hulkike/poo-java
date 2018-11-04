b0339a317fd520ef0c85e0436b1f92d182074efb
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-03 20:58:53 -0500

    chore: ejercicio 06 interface Soldado

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio06/Soldado.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio06;
+
+public interface Soldado extends HormigaInterface {
+    public void luchar();
+    public void defender();
+    public void vigilar();
+}
