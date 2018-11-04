c8586086d9ad9d5a7f043fb3a98507e1117e7f99
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-03 20:58:21 -0500

    chore: ejercicio 06 interface HormigaInterface

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio06/HormigaInterface.java
@@ -0,0 +1,10 @@
+package co.edu.sena.ejercicios.ejercicio06;
+
+public interface HormigaInterface {
+    public void comer();
+    public void respirar();
+    public void caminar();
+    public void correr();
+    public void nacer();
+    public void morir();
+}
