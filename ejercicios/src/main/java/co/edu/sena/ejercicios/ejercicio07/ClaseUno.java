c3f78420d87bad6cd14f07baff59d68b35085ce7
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-04 17:57:34 -0500

    chore: ejercicio 07 ClaseCuatro

--- ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio07/ClaseUno.java
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio07/ClaseUno.java
@@ -3,6 +3,8 @@ package co.edu.sena.ejercicios.ejercicio07;
 public class ClaseUno {
     public class ClaseDos {
         public class ClaseTres {
+            public class ClaseCuatro {
+            }
         }
     }
 }
