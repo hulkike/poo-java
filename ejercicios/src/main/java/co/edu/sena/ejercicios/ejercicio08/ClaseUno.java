6d7ae0aff742045ea79dce123c6467ee2f41ca00
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-05 17:50:17 -0500

    chore: ejercicio 08 clases

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio08/ClaseUno.java
@@ -0,0 +1,22 @@
+package co.edu.sena.ejercicios.ejercicio08;
+
+public class ClaseUno {
+    public class ClaseDos {
+        public class ClaseTres {
+            public class ClaseCuatro {
+                public class ClaseCinco {
+                    public class ClaseSeis {
+                        public class ClaseSiete {
+                            public class ClaseOcho {
+                                public class ClaseNueve {
+                                    public class ClaseDiez {
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
