41c027eac3c451f5f89d80d5f8c2154c80a580f2
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-11 15:43:56 -0500

    chore: ejercicio 09 app

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio09/App.java
@@ -0,0 +1,22 @@
+package co.edu.sena.ejercicios.ejercicio09;
+
+public class App {
+    public static void main(String[] args) {
+        ClaseUno claseUno = new ClaseUno();
+        ClaseUno.ClaseDos claseDos = new ClaseUno.ClaseDos();
+        ClaseUno.ClaseDos.ClaseTres claseTres = new ClaseUno.ClaseDos.ClaseTres();
+        ClaseUno.ClaseDos.ClaseTres.ClaseCuatro claseCuatro = new ClaseUno.ClaseDos.ClaseTres.ClaseCuatro();
+        ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco claseCinco =
+                new ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco();
+        ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis claseSeis =
+                new ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis();
+        ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete claseSiete
+                = new ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete();
+        ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete.ClaseOcho claseOcho
+                = new ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete.ClaseOcho();
+        ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete.ClaseOcho.ClaseNueve claseNueve
+                = new ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete.ClaseOcho.ClaseNueve();
+        ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete.ClaseOcho.ClaseNueve.ClaseDiez claseDiez
+                = new ClaseUno.ClaseDos.ClaseTres.ClaseCuatro.ClaseCinco.ClaseSeis.ClaseSiete.ClaseOcho.ClaseNueve.ClaseDiez();
+    }
+}
