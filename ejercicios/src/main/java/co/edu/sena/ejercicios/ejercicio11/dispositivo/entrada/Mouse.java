321d4693dcd008cee4dc49503712768c06433ce1
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-12 18:21:53 -0500

    chore: ejercicio 11 clase Mouse

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/dispositivo/entrada/Mouse.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.dispositivo.entrada;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Mouse extends Parte {
+    public Mouse(String serial, String marca) {
+        super(serial, marca);
+    }
+}
