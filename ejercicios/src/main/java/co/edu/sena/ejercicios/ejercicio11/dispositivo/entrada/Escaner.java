657ae8424fc86b76dedf5c5da66d97a19f9788ec
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-12 18:21:29 -0500

    chore: ejercicio 11 clase Escaner

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/dispositivo/entrada/Escaner.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.dispositivo.entrada;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Escaner extends Parte {
+    public Escaner(String serial, String marca) {
+        super(serial, marca);
+    }
+}
