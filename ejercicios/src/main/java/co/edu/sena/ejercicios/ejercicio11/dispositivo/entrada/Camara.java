d9c443279d9ef71428d557dff017eb745f06f1e6
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-12 18:20:58 -0500

    chore: ejercicio 11 clase Camara

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/dispositivo/entrada/Camara.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.dispositivo.entrada;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Camara extends Parte {
+    public Camara(String serial, String marca) {
+        super(serial, marca);
+    }
+}
