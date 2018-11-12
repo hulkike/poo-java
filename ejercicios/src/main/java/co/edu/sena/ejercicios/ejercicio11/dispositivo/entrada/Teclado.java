d617c75214bd6cedf462c0209ae28e11c479ebc4
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-12 18:20:24 -0500

    chore: ejercicio 11 clase Teclado

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/dispositivo/entrada/Teclado.java
@@ -0,0 +1,10 @@
+package co.edu.sena.ejercicios.ejercicio11.dispositivo.entrada;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Teclado extends Parte {
+
+    public Teclado(String serial, String marca) {
+        super(serial, marca);
+    }
+}
