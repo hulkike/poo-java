648480e48b8896a28a3aa178abe7b2b2832b8ee6
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-17 21:27:18 -0500

    chore: ejercicio 11 clase Impresora

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/dispositivo/salida/Impresora.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.dispositivo.salida;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Impresora extends Parte {
+    public Impresora(String serial, String marca) {
+        super(serial, marca);
+    }
+}
