7833acb5e3779cd15aed15244030d61adc3eb761
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-17 21:30:17 -0500

    chore: ejercicio 11 clase Disipador

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/Disipador.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Disipador extends Parte {
+    public Disipador(String serial, String marca) {
+        super(serial, marca);
+    }
+}
