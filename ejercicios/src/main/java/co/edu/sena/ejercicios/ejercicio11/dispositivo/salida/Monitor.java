d38dc671715497fe21d39a77efc0f73b917776ab
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-17 21:26:43 -0500

    chore: ejercicio 11 clase Monitor

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/dispositivo/salida/Monitor.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.dispositivo.salida;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Monitor extends Parte {
+    public Monitor(String serial, String marca) {
+        super(serial, marca);
+    }
+}
