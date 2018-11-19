14ea66a6f6b571f6fa34e22f5736ea7d3e3de513
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-18 21:49:05 -0500

    chore: ejercicio 11 clase RAM

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/RAM.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class RAM extends Parte {
+    public RAM(String serial, String marca) {
+        super(serial, marca);
+    }
+}
