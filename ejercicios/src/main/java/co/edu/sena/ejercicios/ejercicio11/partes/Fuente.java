63eb9cb9bf0e8f626814583e1e74bbd34a7d581b
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-18 21:49:34 -0500

    chore: ejercicio 11 clase Fuente

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/Fuente.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Fuente extends Parte {
+    public Fuente(String serial, String marca) {
+        super(serial, marca);
+    }
+}
