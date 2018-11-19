44307873a396e34014976844c38e0c0cfb5ea538
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-18 21:53:28 -0500

    chore: ejercicio 11 clase LectorDVD

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/LectorDVD.java
@@ -0,0 +1,10 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class LectorDVD extends Lector {
+
+    public LectorDVD(String serial, String marca, double alto, double largo, double ancho) {
+        super(serial, marca, alto, largo, ancho);
+    }
+}
