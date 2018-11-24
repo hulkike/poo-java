caed397ecfc93675171df8c54b7c68fd4328e786
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-24 11:01:49 -0500

    chore: ejercicio 11 clase LectorBR

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/LectorBR.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+public class LectorBR extends Lector {
+    public LectorBR(String serial, String marca, double alto, double largo, double ancho) {
+        super(serial, marca, alto, largo, ancho);
+    }
+}
