42edcb9c93f13b08ceeb1a4888256e3eed8b6448
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-24 11:00:51 -0500

    chore: ejercicio 11 clase LectorDisquette

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/LectorDisquette.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+public class LectorDisquette extends Lector {
+    public LectorDisquette(String serial, String marca, double alto, double largo, double ancho) {
+        super(serial, marca, alto, largo, ancho);
+    }
+}
