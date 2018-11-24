5b3bc3b8e6cac03fe1ad642db548f88390c296d8
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-24 11:03:01 -0500

    chore: ejercicio 11 clase LectorCD

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/LectorCD.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+public class LectorCD extends Lector {
+    public LectorCD(String serial, String marca, double alto, double largo, double ancho) {
+        super(serial, marca, alto, largo, ancho);
+    }
+}
