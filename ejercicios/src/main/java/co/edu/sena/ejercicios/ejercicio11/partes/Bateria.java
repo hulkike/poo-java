34fe1351c0d1b025a34ff3569ede423b88a748ab
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-25 08:38:33 -0500

    chore: ejercicio 11 clase Bateria

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/Bateria.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class Bateria extends Parte {
+    public Bateria(String serial, String marca) {
+        super(serial, marca);
+    }
+}
