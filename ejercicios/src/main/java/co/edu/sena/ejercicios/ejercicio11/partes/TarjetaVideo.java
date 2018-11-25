a2b403d3084a39b60124be1668bbee69dbffddaa
Enrique Moreno <hemoreno33@misena.edu.co>
2018-11-25 08:37:20 -0500

    chore: ejercicio 11 clase TarjetaVideo

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio11/partes/TarjetaVideo.java
@@ -0,0 +1,9 @@
+package co.edu.sena.ejercicios.ejercicio11.partes;
+
+import co.edu.sena.ejercicios.ejercicio11.Parte;
+
+public class TarjetaVideo extends Parte {
+    public TarjetaVideo(String serial, String marca) {
+        super(serial, marca);
+    }
+}
