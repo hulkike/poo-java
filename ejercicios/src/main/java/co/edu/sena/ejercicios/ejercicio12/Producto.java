41b61745e18aff40ab7d74649f2342a0d9012f0b
Enrique Moreno <hemoreno33@misena.edu.co>
2018-12-02 10:23:10 -0500

    chore: ejercicio 12 clase Producto

---
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio12/Producto.java
@@ -0,0 +1,74 @@
+package co.edu.sena.ejercicios.ejercicio12;
+
+import java.util.Objects;
+
+public class Producto {
+    private String idProducto;
+    private String nombre;
+    private double precio;
+    private int cantidadDisponible;
+    private Item item;
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(idProducto, producto.idProducto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idProducto);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "idProducto='" + idProducto + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", cantidadDisponible=" + cantidadDisponible +
+                '}';
+    }
+}
