ff7ed06ac32cf21fe907e4f6657971d7cf26d002
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-05-07 21:32:19 -0500

    chore: ejercicio 02 se termina

--- ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/App.java
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/App.java
@@ -11,7 +11,6 @@ public class App {
         sapoMacho.reproducirse();
         sapoMacho.respirar();
 
-
         SapoHembra saporHembra = new SapoHembra();
         System.out.println("---Sapo Hembra---");
         saporHembra.comer();
@@ -21,6 +20,197 @@ public class App {
         saporHembra.reproducirse();
         saporHembra.respirar();
         saporHembra.criar();
+
+        OrnitorrincoMacho ornitorrincoMacho = new OrnitorrincoMacho();
+        System.out.println("---Ornitorrinco Macho---");
+        ornitorrincoMacho.comer();
+        ornitorrincoMacho.desplazarse();
+        ornitorrincoMacho.morir();
+        ornitorrincoMacho.nacer();
+        ornitorrincoMacho.reproducirse();
+        ornitorrincoMacho.respirar();
+
+        OrnitorrincoHembra ornitorrincoHembra = new OrnitorrincoHembra();
+        System.out.println("---Ornitorrinco Hembra---");
+        ornitorrincoHembra.comer();
+        ornitorrincoHembra.desplazarse();
+        ornitorrincoHembra.morir();
+        ornitorrincoHembra.nacer();
+        ornitorrincoHembra.reproducirse();
+        ornitorrincoHembra.respirar();
+        ornitorrincoHembra.criar();
+
+        AvestruzMacho avestruzMacho = new AvestruzMacho();
+        System.out.println("--- Avestruz Macho---");
+        avestruzMacho.comer();
+        avestruzMacho.desplazarse();
+        avestruzMacho.morir();
+        avestruzMacho.nacer();
+        avestruzMacho.reproducirse();
+        avestruzMacho.respirar();
+
+        AvestruzHembra avestruzHembra = new AvestruzHembra();
+        System.out.println("--- Avestruz Hembra---");
+        avestruzHembra.comer();
+        avestruzHembra.desplazarse();
+        avestruzHembra.morir();
+        avestruzHembra.nacer();
+        avestruzHembra.reproducirse();
+        avestruzHembra.respirar();
+        avestruzHembra.criar();
+
+        TortugaMacho tortugaMacho = new TortugaMacho();
+        System.out.println("--- Tortuga Macho---");
+        tortugaMacho.comer();
+        tortugaMacho.desplazarse();
+        tortugaMacho.morir();
+        tortugaMacho.nacer();
+        tortugaMacho.reproducirse();
+        tortugaMacho.respirar();
+
+        TortugaHembra tortugaHembra = new TortugaHembra();
+        System.out.println("--- Avestruz Hembra---");
+        tortugaHembra.comer();
+        tortugaHembra.desplazarse();
+        tortugaHembra.morir();
+        tortugaHembra.nacer();
+        tortugaHembra.reproducirse();
+        tortugaHembra.respirar();
+        tortugaHembra.criar();
+
+        CocodrilloMacho cocodrilloMacho = new CocodrilloMacho();
+        System.out.println("--- Cocodrilo Macho---");
+        cocodrilloMacho.comer();
+        cocodrilloMacho.desplazarse();
+        cocodrilloMacho.morir();
+        cocodrilloMacho.nacer();
+        cocodrilloMacho.reproducirse();
+        cocodrilloMacho.respirar();
+
+        CocodriloHembra cocodriloHembra = new CocodriloHembra();
+        System.out.println("--- Cocodrilo Hembra---");
+        cocodriloHembra.comer();
+        cocodriloHembra.desplazarse();
+        cocodriloHembra.morir();
+        cocodriloHembra.nacer();
+        cocodriloHembra.reproducirse();
+        cocodriloHembra.respirar();
+        cocodriloHembra.criar();
+
+        Hombre hombre = new Hombre();
+        System.out.println("--- Hombre---");
+        hombre.comer();
+        hombre.desplazarse();
+        hombre.morir();
+        hombre.nacer();
+        hombre.reproducirse();
+        hombre.respirar();
+
+        Mujer mujer = new Mujer();
+        System.out.println("--- Mujer---");
+        mujer.comer();
+        mujer.desplazarse();
+        mujer.morir();
+        mujer.nacer();
+        mujer.reproducirse();
+        mujer.respirar();
+        mujer.criar();
+
+        AtunMacho atunMacho = new AtunMacho();
+        System.out.println("---Atun Macho---");
+        atunMacho.comer();
+        atunMacho.desplazarse();
+        atunMacho.morir();
+        atunMacho.nacer();
+        atunMacho.reproducirse();
+        atunMacho.respirar();
+
+        AtunHembra atunHembra = new AtunHembra();
+        System.out.println("---Atun Hembra---");
+        atunHembra.comer();
+        atunHembra.desplazarse();
+        atunHembra.morir();
+        atunHembra.nacer();
+        atunHembra.reproducirse();
+        atunHembra.respirar();
+        atunHembra.criar();
+
+        TiburonMacho tiburonMacho = new TiburonMacho();
+        System.out.println("---Tiburon Macho---");
+        tiburonMacho.comer();
+        tiburonMacho.desplazarse();
+        tiburonMacho.morir();
+        tiburonMacho.nacer();
+        tiburonMacho.reproducirse();
+        tiburonMacho.respirar();
+
+        TiburonHembra tiburonHembra = new TiburonHembra();
+        System.out.println("---Tiburon Hembra---");
+        tiburonHembra.comer();
+        tiburonHembra.desplazarse();
+        tiburonHembra.morir();
+        tiburonHembra.nacer();
+        tiburonHembra.reproducirse();
+        tiburonHembra.respirar();
+        tiburonHembra.criar();
+
+        SalamandraMacho salamandraMacho = new SalamandraMacho();
+        System.out.println("---salamandra macho---");
+        salamandraMacho.comer();
+        salamandraMacho.desplazarse();
+        salamandraMacho.morir();
+        salamandraMacho.nacer();
+        salamandraMacho.reproducirse();
+        salamandraMacho.respirar();
+
+        SalamandraHembra salamandraHembra = new SalamandraHembra();
+        System.out.println("---salamandra hembra---");
+        salamandraHembra.comer();
+        salamandraHembra.desplazarse();
+        salamandraHembra.morir();
+        salamandraHembra.nacer();
+        salamandraHembra.reproducirse();
+        salamandraHembra.respirar();
+        salamandraHembra.criar();
+
+        AguilaMacho aguilaMacho = new AguilaMacho();
+        System.out.println("---Águila Macho---");
+        aguilaMacho.comer();
+        aguilaMacho.desplazarse();
+        aguilaMacho.morir();
+        aguilaMacho.nacer();
+        aguilaMacho.reproducirse();
+        aguilaMacho.respirar();
+
+        AguilaHembra aguilaHembra = new AguilaHembra();
+        System.out.println("---Águila Hembra---");
+        aguilaHembra.comer();
+        aguilaHembra.desplazarse();
+        aguilaHembra.morir();
+        aguilaHembra.nacer();
+        aguilaHembra.reproducirse();
+        aguilaHembra.respirar();
+        aguilaHembra.criar();
+
+        PerroMacho perroMacho = new PerroMacho();
+        System.out.println("---Perro macho---");
+        perroMacho.comer();
+        perroMacho.desplazarse();
+        perroMacho.morir();
+        perroMacho.nacer();
+        perroMacho.reproducirse();
+        perroMacho.respirar();
+
+        PerroHembra perroHembra = new PerroHembra();
+        System.out.println("---Perro Hembra---");
+        perroHembra.comer();
+        perroHembra.desplazarse();
+        perroHembra.morir();
+        perroHembra.nacer();
+        perroHembra.reproducirse();
+        perroHembra.respirar();
+        perroHembra.criar();
+
 
 
 
--- ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/CocodriloHembra.java
+++ ejercicios/src/main/java/co/edu/sena/ejercicios/ejercicio02/CocodriloHembra.java
@@ -1,6 +1,6 @@
 package co.edu.sena.ejercicios.ejercicio02;
 
-public class CocodriloHembra {
+public class CocodriloHembra extends Cocodrilo {
     public void criar(){
         System.out.println("pongo huevos y cuido mis crias");
     }
